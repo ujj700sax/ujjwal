8f6d47776731bccfa92b4946c247e6cbe09ac400
qainfotech <qainfotech@APLA0008.local>
2020-05-17 14:37:19 +0530

    added

---
+++ src/test/java/com/qa/test/demo/testdmo.java
@@ -0,0 +1,16 @@
+package com.qa.test.demo;
+
+public class testdmo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test();
+	}
+	
+		 static void test() {
+			System.out.println("123ty");
+		}
+		
+	}
+
+
